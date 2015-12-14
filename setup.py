dbcebab7ebcf505769cf53171d4928f1601095aa
davisben85 <davisben85@gmail.com>
2015-12-14 14:26:01 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='app',
+      version='0.1',
+      description='test app',
+      url='https://github.com/davisben85/csibootcamp',
+      author='Ben Davis',
+      author_email='davisben85@gmail.com',
+      license='MIT',
+      packages=['app'],
+      install_requires[
+        'pip'
+      ],
+      zip_safe=False)
+      
