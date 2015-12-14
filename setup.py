91d56b13cd835fc1c11aa7f89b295532c9cec39f
davisben85 <davisben85@gmail.com>
2015-12-14 15:12:24 -0500

    Added check for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-from setuptools import setup
+modules = set(['pip'])
 
-setup(name='app',
-      version='0.1',
-      description='test app',
-      url='https://github.com/davisben85/csibootcamp',
-      author='Ben Davis',
-      author_email='davisben85@gmail.com',
-      license='MIT',
-      packages=['app'],
-      install_requires=['pip'],
-      zip_safe=False)
-      
+for module in modules:
+      try:
+            __import__(module):
+      except ImportError:
+            break
+                        
